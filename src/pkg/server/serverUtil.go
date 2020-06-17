067d15d941d2a7cbc8fec4eb7b664eeb083e13bd
Manuel Alex Perez <manuel.parez@toyota.com>
2020-06-17 11:56:07 -0400

    structured logs for serverUtil.go

--- src/pkg/server/serverUtil.go
+++ src/pkg/server/serverUtil.go
@@ -1,12 +1,16 @@
 package server
 
-import "net/http"
+import (
+	"github.com/Perezonance/bnr-assignment/src/pkg/util"
+	"net/http"
+)
 
 func writeRes(statusCode int, message string, w http.ResponseWriter) {
 	w.WriteHeader(statusCode)
 	res := []byte(message)
 	_, err := w.Write(res)
 	if err != nil {
-		//TODO: ERROR HANDLING
+		util.ErrorLog("Failed to write response", err)
 	}
+	return
 }
